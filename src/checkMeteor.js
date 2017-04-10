e76c04c5bd8818a7a220a8cb4556a21fc4ed91f0
Max Nowack <max@unsou.de>
2017-04-10 16:31:42 +0200

    do nothing on client or in production mode

--- src/checkMeteor.js
+++ src/checkMeteor.js
@@ -1,8 +1,13 @@
 import path from 'path';
 import fs from 'fs';
+import getGlobal from './getGlobal';
 
 const checks = {
   fileCheck() {
+    if (process.env.NODE_ENV === 'production') return true;
+    const meteor = getGlobal('meteor', 'Meteor');
+    if (!meteor) return false;
+    if (meteor.isClient) return true;
     const packageFile = path.resolve('./.meteor/packages');
     return fs.existsSync(packageFile);
   },
